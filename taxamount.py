2bf75d73e1fa60ea0b66ac1bc8823bb3e5bd1d16
awoiibob <awoiibobwilly@gmail.com>
2025-04-05 12:32:31 +0300

    Successful Function to be tested

--- taxamount.py
+++ taxamount.py
@@ -1,11 +1,22 @@
 import unittest
 
 
+def taxamount(income, no_of_months):
+    if income < 1000:
+        return 0.0
+    elif income < 3000:
+        return round(income * 0.2 * no_of_months, 1)
+    else:
+        return round(income * 0.4 * no_of_months, 1)
+
+
+# print(taxamount(5000, 3))  # should return 6000.0
+
 class TestTaxAmount(unittest.TestCase):
     def test_amount1(self):
-        self.assertEqual(taxamount(999, 3), 0)
-        self.assertEqual(taxamount(1001, 3), 600.6)
-        self.assertEqual(taxamount(3001, 3), 3601.2)
+        self.assertEqual(taxamount(999.9, 3), 0.0)
+        self.assertEqual(taxamount(2500, 3), 1500.0)
+        self.assertEqual(taxamount(4500, 3), 5400.0)
 
 
 if __name__ == '__main__':
